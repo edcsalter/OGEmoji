c3153da942437c56fffa0c7750c0f951592cf3d3
Ed <edcsalter@gmail.com>
2020-08-14 17:16:31 -0400

    Project birth

---
+++ OG-Emoji/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OG-Emoji
+//
+//  Created by Edward Salter on 8/14/20.
+//  Copyright © 2020 theiOSdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
